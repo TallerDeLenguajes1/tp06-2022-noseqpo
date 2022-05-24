a0fd1b9997b22c55a3b455ee5cb815d3b4d3a38d
Daniel Paz <danielnicolaspeace@gmail.com>
2022-05-24 15:32:30 -0300

    primer punto casi completo

---
+++ TP6/Calculadora.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP6
+{
+    
+
+    public class Calculadora
+    {
+        private double operado;
+
+        public Calculadora(double operado)
+        {
+            this.operado = operado;
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("\nEl valor actual es: ");
+            Console.WriteLine(operado);
+        }
+
+        public void Limpiar()
+        {
+            this.operado = 0;
+        }
+
+        public static double Solicitar()
+        {
+            Console.WriteLine("\nIngrese un valor: ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            return temp;
+        }
+
+        public double Sumar(double termino)
+        {
+            operado += termino;
+            return operado;
+        }
+
+        public double Restar(double termino)
+        {
+            operado -= termino;
+            return operado;
+        }
+        public double Muktiplicar(double termino)
+        {
+            operado *= termino;
+            return operado;
+        }
+        public double Dividir(double termino)
+        {
+            operado /= termino;
+            return operado;
+        }
+
+    }
+}
---
+++ TP6/Program.cs
@@ -0,0 +1,30 @@
+﻿using TP6;
+
+Console.WriteLine("Indique un valor con el cual trabajar: ");
+double oper = Convert.ToDouble(Console.ReadLine());
+Calculadora numero = new Calculadora(oper);
+string lpr = "S";
+
+while (lpr == "S")
+{
+    Console.WriteLine("\nIndique la operacion deseada: \n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division");
+    int eleccion = Convert.ToInt32(Console.ReadLine()); 
+    Console.WriteLine("\nIndique el valor por el que desea operar: ");
+    double term = Calculadora.Solicitar();
+    switch (eleccion)
+    {
+        case 1: numero.Sumar(term); break;
+        case 2: numero.Restar(term); break;
+        case 3: numero.Muktiplicar(term); break;
+        case 4: numero.Dividir(term); break;
+        default: Console.WriteLine("Error"); break;
+    }
+    numero.Mostrar();
+    
+    Console.WriteLine("Desea realizar otra operacion? S/N");
+    lpr = Console.ReadLine();
+    if(lpr != "N")
+    {
+        lpr = "S";
+    }
+}
